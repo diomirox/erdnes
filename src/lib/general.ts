2060770297c12b2e033ad97fa8e45ed8808964bd
cybinon <nikorunikk@gmail.com>
2023-10-30 11:07:08 +0800

    fix: bundle issue fix

--- src/lib/general.ts
+++ src/lib/general.ts
@@ -1,15 +1,16 @@
 /**
  * Checks if all elements in the headers array are equal to the corresponding elements in the buffers array.
  *
- * @param {any} headers - The array of headers to compare.
+ * @param {number[]} headers - The array of headers to compare.
  * @return {boolean} Returns true if all elements in the headers array are equal to the corresponding elements in the buffers array, otherwise returns false.
  */
 export function check(
   headers: number[]
-): (buffers: Iterable<number>, options?: any) => boolean {
+): (buffers: Iterable<number>, options?: { offset: number }) => boolean {
   return (buffers: Iterable<number>, options = { offset: 0 }) =>
     headers.every(
-      (header: any, index: any) => header === buffers[options.offset + index]
+      (header, index) =>
+        header === (buffers as number[])[options.offset + index]
     );
 }
 
